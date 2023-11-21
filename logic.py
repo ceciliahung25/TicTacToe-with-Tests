36ef1c444acaeb474fae1f4204070fa67cedc4bd
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-20 22:38:12 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -16,29 +16,28 @@ class Board:
         ]
 
     def other_player(self, player):
-        if player == 'X':
-            return 'O'
-        else:
-            return 'X'
+        return 'O' if player == 'X' else 'X'
 
     def get_winner(self):
         # Check rows
         for row in self.grid:
-            if row[0] == row[1] == row[2] and row[0] is not None:
+            if self.check_line(row):
                 return row[0]
 
         # Check columns
         for col in range(3):
-            if self.grid[0][col] == self.grid[1][col] == self.grid[2][col] and self.grid[0][col] is not None:
+            if self.check_line([self.grid[row][col] for row in range(3)]):
                 return self.grid[0][col]
 
         # Check diagonals
-        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] and self.grid[0][0] is not None:
-            return self.grid[0][0]
-        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[0][2] is not None:
-            return self.grid[0][2]
+        if self.check_line([self.grid[i][i] for i in range(3)]) or \
+           self.check_line([self.grid[i][2 - i] for i in range(3)]):
+            return self.grid[1][1]
 
         return None
+
+    def check_line(self, line):
+        return all(cell == line[0] and cell is not None for cell in line)
 
     def get_empty_squares(self):
         return [(i, j) for i in range(3) for j in range(3) if self.grid[i][j] is None]
@@ -50,3 +49,4 @@ class RandomBot:
     def get_move(self, board):
         available_squares = board.get_empty_squares()
         return random.choice(available_squares) if available_squares else None
+
