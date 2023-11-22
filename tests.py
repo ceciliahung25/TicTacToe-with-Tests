956a0296dd0122648b8b1e2e1acdd2f58c3f6849
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 22:17:33 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from logic import Board, RandomBot, choose_player_type
 class TestTicTacToe(unittest.TestCase):
 
     def test_correct_game_winner_detected(self):
-        board = Board()  # 新的空白棋盘
+        board = Board()  # New empty board
         board.grid = [
             ['X', None, None],
             [None, None, None],
@@ -14,33 +14,33 @@ class TestTicTacToe(unittest.TestCase):
         self.assertEqual(board.get_winner(), 'X')
 
     def test_players_can_play_only_in_viable_spots(self):
-        board = Board()  # 新的空白棋盘
-        row, col = 0, 0  # 假设 (0, 0) 是一个非空位置
-
+        board = Board()  # New empty board
+        row, col = 0, 0  # Assume (0, 0) is a non-empty spot
         with self.assertRaises(ValueError):
             # Attempt to place 'X' in a non-empty spot
             board.grid[row][col] = 'X'
+            board.grid[row][col] = 'X'  # This line should raise a ValueError
 
     def test_game_initialized_with_empty_board(self):
-        board = Board()  # 新的空白棋盘
-        self.assertIsNone(board.get_winner())  # 确保游戏开始时没有获胜者
-        self.assertTrue(all(cell is None for row in board.grid for cell in row))  # 所有单元格都应该是空的
+        board = Board()  # New empty board
+        self.assertIsNone(board.get_winner())  # Ensure no winner at the start
+        self.assertTrue(all(cell is None for row in board.grid for cell in row))  # All cells should be empty
 
     @patch('builtins.input', side_effect=['1'])
     def test_game_initialized_with_two_players(self, mock_input):
-        player_type = choose_player_type()  # 模拟用户输入 1，表示选择人类玩家
-        self.assertEqual(player_type, 'X')  # 确保选择的玩家类型是 'X'
+        player_type = choose_player_type()  # Simulate user input 1, indicating choosing a human player
+        self.assertEqual(player_type, 'X')  # Ensure the chosen player type is 'X'
 
     @patch('builtins.input', side_effect=['2'])
     def test_game_initialized_with_one_player(self, mock_input):
-        player_type = choose_player_type()  # 模拟用户输入 2，表示选择 RandomBot 玩家
-        self.assertEqual(player_type, 'O')  # 确保选择的玩家类型是 'O'
+        player_type = choose_player_type()  # Simulate user input 2, indicating choosing a RandomBot player
+        self.assertEqual(player_type, 'O')  # Ensure the chosen player type is 'O'
 
     def test_players_assigned_unique_piece(self):
-        board = Board()  # 新的空白棋盘
+        board = Board()  # New empty board
         player1_type = 'X'
         player2_type = 'O'
-        self.assertNotEqual(player1_type, player2_type)  # 确保两个玩家被分配不同的棋子类型
+        self.assertNotEqual(player1_type, player2_type)  # Ensure both players are assigned different piece types
 
 if __name__ == '__main__':
     unittest.main()
