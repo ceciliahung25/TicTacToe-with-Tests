7528e381ce26b08db7e5b6fce96cc042c1207ab6
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 21:54:08 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,69 +1,51 @@
-import unittest
-from logic import Board, RandomBot
-from cli import choose_player_type
-from unittest.mock import patch
-
-class TestTicTacToe(unittest.TestCase):
-
-    def test_game_initialized_with_empty_board(self):
-        board = Board()
-        self.assertEqual(board.grid, [[None, None, None], [None, None, None], [None, None, None]])
-
-    def test_players_assigned_unique_piece(self):
-        with patch('builtins.input', side_effect=['1']):
-            player1 = choose_player_type()
-        with patch('builtins.input', side_effect=['2']):
-            player2 = choose_player_type()
-        self.assertNotEqual(player1, player2)
-
-    def test_after_one_player_plays_other_has_turn(self):
-        board = Board()
-        with patch('builtins.input', side_effect=['1']):
-            player1 = choose_player_type()
-        player2 = board.other_player(player1)
-        self.assertNotEqual(player1, player2)
-
-    def test_correct_game_winner_detected(self):
-        board = Board()
-        # Set up a winning scenario
-        board.grid = [
-            ['X', 'O', 'X'],
-            [None, 'O', None],
-            ['O', None, 'X'],
+class Board:
+    def __init__(self):
+        self.grid = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
         ]
-        print(board.get_winner())  # Add this line to debug
-        self.assertEqual(board.get_winner(), 'X')
 
-    def test_draw_games_identified(self):
-        board = Board()
-        # Set up a draw scenario
-        board.grid = [
-            ['X', 'O', 'X'],
-            ['X', 'O', 'O'],
-            ['O', 'X', 'X'],
+    def make_empty_board(self):
+        self.grid = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
         ]
-        self.assertTrue(all(cell is not None for row in board.grid for cell in row))
-        self.assertIsNone(board.get_winner())
 
-    @patch('builtins.input', side_effect=['1'])
-    def test_players_can_play_only_in_viable_spots(self, mock_input):
-        board = Board()
-        player = choose_player_type()
-        row, col = 0, 0
-        board.grid[row][col] = 'X'
-        with self.assertRaises(ValueError):
-            board.grid[row][col] = player
+    def other_player(self, player):
+        return 'O' if player == 'X' else 'X'
 
-    def test_winning_end_of_games_detected(self):
-        board = Board()
-        # Set up a winning scenario
-        board.grid = [
-            ['X', 'O', 'X'],
-            [None, 'O', None],
-            ['O', None, 'X'],
-        ]
-        print(board.get_winner())  # Add this line to debug
-        self.assertEqual(board.get_winner(), 'X')
+    def get_winner(self):
+        # Check rows
+        for row in self.grid:
+            if self.check_line(row):
+                return row[0]
 
-if __name__ == '__main__':
-    unittest.main()
+        # Check columns
+        for col in range(3):
+            if self.check_line([self.grid[row][col] for row in range(3)]):
+                return self.grid[0][col]
+
+        # Check diagonals
+        if self.check_line([self.grid[i][i] for i in range(3)]):
+            return self.grid[0][0]
+        elif self.check_line([self.grid[i][2 - i] for i in range(3)]):
+            return self.grid[0][2]
+
+        return None
+
+    def check_line(self, line):
+        return all(cell == line[0] and cell is not None for cell in line) and line[0] is not None
+
+    def get_empty_squares(self):
+        return [(i, j) for i in range(3) for j in range(3) if self.grid[i][j] is None]
+
+
+class RandomBot:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+        available_squares = board.get_empty_squares()
+        return random.choice(available_squares) if available_squares else None
