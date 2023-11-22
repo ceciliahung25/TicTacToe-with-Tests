66cc1cd9b058dff3f1c708ecbd3666d682d3a031
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 22:48:06 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -28,20 +28,19 @@ class Board:
         for row in self.grid:
             if row[0] == row[1] == row[2] and row[0] is not None:
                 return row[0]
-
+    
         # Check columns
         for col in range(3):
             if self.grid[0][col] == self.grid[1][col] == self.grid[2][col] and self.grid[0][col] is not None:
                 return self.grid[0][col]
-
+    
         # Check diagonals
         if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] and self.grid[0][0] is not None:
             return self.grid[0][0]
         elif self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[0][2] is not None:
             return self.grid[0][2]
-
+    
         return None
-
 
 class RandomBot:
     def __init__(self, symbol):
