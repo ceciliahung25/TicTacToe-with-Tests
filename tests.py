705e1e5b4c265fa9415b1c005583c5dc9e80bb64
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 22:33:13 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from logic import Board, RandomBot, choose_player_type
 class TestTicTacToe(unittest.TestCase):
 
     def test_correct_game_winner_detected(self):
-        board = Board()
+        board = Board()  # New empty board
         board.grid = [
             ['X', None, None],
             [None, None, None],
@@ -14,13 +14,13 @@ class TestTicTacToe(unittest.TestCase):
         self.assertEqual(board.get_winner(), 'X')
 
     def test_players_can_play_only_in_viable_spots(self):
-        board = Board()
-        row, col = 0, 0
+        board = Board()  # New empty board
+        row, col = 0, 0  # Assume (0, 0) is a non-empty spot
         with self.assertRaises(ValueError):
             board.make_move('X', row, col)
 
     def test_game_initialized_with_empty_board(self):
-        board = Board()
+        board = Board()  # New empty board
         self.assertIsNone(board.get_winner())
         self.assertTrue(all(cell is None for row in board.grid for cell in row))
 
@@ -35,20 +35,10 @@ class TestTicTacToe(unittest.TestCase):
         self.assertEqual(player_type, 'O')
 
     def test_players_assigned_unique_piece(self):
-        board = Board()
+        board = Board()  # New empty board
         player1_type = 'X'
         player2_type = 'O'
         self.assertNotEqual(player1_type, player2_type)
-
-    def test_player_cannot_play_in_occupied_spot(self):
-        board = Board()
-        board.grid = [
-            ['X', 'O', None],
-            [None, None, None],
-            [None, None, None],
-        ]
-        with self.assertRaises(ValueError):
-            board.make_move('X', 0, 0)
 
 if __name__ == '__main__':
     unittest.main()
