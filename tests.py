7c37108272283dbe6fc826d95fc00ea74fa84002
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 22:23:37 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,41 +5,40 @@ from logic import Board, RandomBot, choose_player_type
 class TestTicTacToe(unittest.TestCase):
 
     def test_correct_game_winner_detected(self):
-        board = Board()  # New empty board
+        board = Board()  
         board.grid = [
-            ['X', None, None],
+            ['X', 'X', 'X'],
             [None, None, None],
             [None, None, None],
         ]
         self.assertEqual(board.get_winner(), 'X')
 
     def test_players_can_play_only_in_viable_spots(self):
-        board = Board()  # New empty board
-        row, col = 0, 0  # Assume (0, 0) is a non-empty spot
+        board = Board()  
+        row, col = 0, 0  
         with self.assertRaises(ValueError):
-            # Attempt to place 'X' in a non-empty spot
-            board.grid[row][col] = 'X'  # This line should raise a ValueError
+            board.grid[row][col] = 'X'  
 
     def test_game_initialized_with_empty_board(self):
-        board = Board()  # New empty board
-        self.assertIsNone(board.get_winner())  # Ensure no winner at the start
-        self.assertTrue(all(cell is None for row in board.grid for cell in row))  # All cells should be empty
+        board = Board()  
+        self.assertIsNone(board.get_winner())
+        self.assertTrue(all(cell is None for row in board.grid for cell in row))
 
     @patch('builtins.input', side_effect=['1'])
     def test_game_initialized_with_two_players(self, mock_input):
-        player_type = choose_player_type()  # Simulate user input '1' for human player
-        self.assertEqual(player_type, 'X')  # Ensure the selected player type is 'X'
+        player_type = choose_player_type()  
+        self.assertEqual(player_type, 'X')
 
     @patch('builtins.input', side_effect=['2'])
     def test_game_initialized_with_one_player(self, mock_input):
-        player_type = choose_player_type()  # Simulate user input '2' for RandomBot player
-        self.assertEqual(player_type, 'O')  # Ensure the selected player type is 'O'
+        player_type = choose_player_type()  
+        self.assertEqual(player_type, 'O')
 
     def test_players_assigned_unique_piece(self):
-        board = Board()  # New empty board
+        board = Board()  
         player1_type = 'X'
         player2_type = 'O'
-        self.assertNotEqual(player1_type, player2_type)  # Ensure different piece types for players
+        self.assertNotEqual(player1_type, player2_type)
 
 if __name__ == '__main__':
     unittest.main()
