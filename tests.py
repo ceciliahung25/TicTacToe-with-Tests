312ea22e979a8c738e53aba74bd0e92a6d135b38
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-21 22:36:03 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,8 +8,8 @@ class TestTicTacToe(unittest.TestCase):
         board = Board()  # New empty board
         board.grid = [
             ['X', None, None],
-            [None, None, None],
-            [None, None, None],
+            [None, 'X', None],
+            [None, None, 'X'],
         ]
         self.assertEqual(board.get_winner(), 'X')
 
@@ -21,24 +21,24 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_game_initialized_with_empty_board(self):
         board = Board()  # New empty board
-        self.assertIsNone(board.get_winner())
-        self.assertTrue(all(cell is None for row in board.grid for cell in row))
+        self.assertIsNone(board.get_winner())  # Ensure no winner at the start
+        self.assertTrue(all(cell is None for row in board.grid for cell in row))  # All cells should be empty
 
     @patch('builtins.input', side_effect=['1'])
     def test_game_initialized_with_two_players(self, mock_input):
-        player_type = choose_player_type()
-        self.assertEqual(player_type, 'X')
+        player_type = choose_player_type()  # Simulate user input 1, indicating a human player
+        self.assertEqual(player_type, 'X')  # Ensure the selected player type is 'X'
 
     @patch('builtins.input', side_effect=['2'])
     def test_game_initialized_with_one_player(self, mock_input):
-        player_type = choose_player_type()
-        self.assertEqual(player_type, 'O')
+        player_type = choose_player_type()  # Simulate user input 2, indicating a RandomBot player
+        self.assertEqual(player_type, 'O')  # Ensure the selected player type is 'O'
 
     def test_players_assigned_unique_piece(self):
         board = Board()  # New empty board
         player1_type = 'X'
         player2_type = 'O'
-        self.assertNotEqual(player1_type, player2_type)
+        self.assertNotEqual(player1_type, player2_type)  # Ensure two players are assigned different piece types
 
 if __name__ == '__main__':
     unittest.main()
